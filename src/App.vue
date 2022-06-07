109a99a5a166acba4ac29f2c50e242394b154604
Darko Izgarevic <darko.izgarevic@oryxgaming.com>
2022-06-07 16:10:18 +0200

    Add all my files

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Pozivnica" src="./assets/pozivnica.jpeg">
+    <p>Molimo potvrdite vaš dolazak: </p>
+    <contact-form />
+  </div>
+</template>
+
+<script>
+import ContactForm from './components/ContactForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ContactForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  width: 700px;
+  padding-bottom: 60px;
+}
+</style>
